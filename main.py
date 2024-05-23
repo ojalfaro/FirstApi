b98658d1a64279e2259bc2012a1e8389c2df51c8
javier <ojalfarob@gmail.com>
2024-05-23 14:41:03 -0600

    add method get

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi  import FastAPI
+from fastapi  import FastAPI,HTTPException
 from pydantic import BaseModel
 from typing import List,Optional
 from uuid import UUID,uuid4
@@ -23,8 +23,15 @@ def create_task(task: Task):
 
 
 @app.get("/tasks/",response_model=List[Task])
-def read_task():
+def read_tasks():
     return tasks
+
+@app.get("/tasks/{task_id}",response_model=Task)
+def read_task(task_id:UUID):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+    return HTTPException(status_code=404,detail="task not found")
 
 
 
